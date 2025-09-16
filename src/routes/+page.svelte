f351b2f5931e47de1618eefb53f34c3907192073
Gesart Mlloja <gesmll20@htl-shkoder.com>
2025-09-16 11:01:00 +0200

    3.commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,83 @@
 <script>
 	import { onMount } from 'svelte';
 
-	let latitude = '';
-	let longitude = '';
-	let accuracy = '';
+	let address = '';
 	let errorText = '';
+	let loading = false;
+	let flagUrl = '';
 
-	function showPosition(position) {
-		latitude  = position.coords.latitude;
-		longitude = position.coords.longitude;
-		accuracy  = position.coords.accuracy;
-	}
+	async function fetchAddress(latitude, longitude) {
+		try {
+			const res = await fetch(
+				`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+			);
+			if (!res.ok) throw new Error('Fehler beim Abrufen der Adressdaten.');
+			const data = await res.json();
 
-	function showError(error) {
-		errorText = error.message;
-	}
+			const addr = data.address || {};
+			address = [
+				addr.road,
+				addr.house_number,
+				addr.postcode,
+				addr.city || addr.town || addr.village,
+				addr.state,
+				addr.country
+			]
+				.filter(Boolean)
+				.join(', ');
 
-	onMount(() => {
-		if ('geolocation' in navigator) {
-			navigator.geolocation.getCurrentPosition(showPosition, showError);
-		} else {
-			errorText = 'Geolocation wird nicht unterstützt.';
+			if (addr.country) {
+			const response = await fetch(
+				`https://restcountries.com/v3.1/name/${encodeURIComponent(
+					addr.country
+				)}?fullText=true`
+			);
+			if (!response.ok) throw new Error('Error retrieving flag.');
+			const countryData = await response.json();
+
+			flagUrl = countryData[0]?.flags?.svg || '';
 		}
-	});
+		} catch (err) {
+			errorText = err.message;
+		}
+	}
+		
+	
+	function getLocation() {
+		errorText = '';
+		address = '';
+		loading = true;
+
+		if ('geolocation' in navigator) {
+			navigator.geolocation.getCurrentPosition(
+				(position) => fetchAddress(position.coords.latitude, position.coords.longitude),
+				(error) => {
+					errorText = error.message;
+					loading = false;
+				}
+			);
+		} else {
+			errorText = 'Geolocation is not supported.';
+			loading = false;
+		}
+	}
 </script>
 
-<h1>Standort</h1>
+<h1>My Location </h1>
+<button on:click={getLocation}
+	class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+	Get Location
+</button>
 
 {#if errorText}
-	<p>Fehler: {errorText}</p>
-{:else if latitude}
-	<p>Breitengrad: {latitude}</p>
-	<p>Längengrad: {longitude}</p>
-	<p>Genauigkeit (m): {accuracy}</p>
+	<p style="color:red;">Error: {errorText}</p>
+{:else if address}
+	<p><strong>Address:</strong> {address}</p>
+
+{#if flagUrl}
+	<p><strong>Flag:</strong></p>
+	<img src={flagUrl} alt="LandFlag" style="width:150px;height:auto;" />
+{/if}
 {:else}
-	<p>Lade Standortdaten …</p>
+	<p>Click on “Get location” to see your address and flag.</p>
 {/if}
